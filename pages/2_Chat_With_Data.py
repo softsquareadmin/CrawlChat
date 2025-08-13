86d8501d967f1f125a0380880a8a2103a385b6ba
Manikandan P <manip@Manikandans-MacBook-Air.local>
2025-08-13 14:00:46 +0530

    UI chnages

--- pages/2_Chat_With_Data.py
+++ pages/2_Chat_With_Data.py
@@ -19,6 +19,20 @@ st.markdown("""
     
 """, unsafe_allow_html=True)
 
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    ._container_gzau3_1, ._profileContainer_gzau3_53,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
  
 # ---- CSS ----
 st.markdown("""
